aeaf26f7026571c4938e265e4565a7f6cd7ab277
JaFiGo <orlfigueroagonzalez@gmail.com>
2022-12-29 20:57:09 -0300

    Update UserController.java
    Comentarios finales

--- src/main/java/com/javier/prueba/controller/UserController.java
+++ src/main/java/com/javier/prueba/controller/UserController.java
@@ -15,33 +15,38 @@ import com.javier.prueba.service.UserService;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-	
+	//llamamos UserService para utilizar sus funciones y utilizar la lista de usuarios
 	private final UserService service;
 	
 	public UserController(UserService service) {
 		this.service = service;
 	}
 	
+	//Solicitud Get para obtener la lista de usuarios 
 	@GetMapping
 	public Iterable<User> list(){
 		return service.list();
 	}
 	
+	//Solicitud Get para obtener el usuario deseado especificamente por su userID y su id
 	@GetMapping("/{uid}/{id}")
 	public User find (@PathVariable("uid") int uid, @PathVariable("id") int id) {
 		return service.find(uid,id);
 	}
 	
+	//Solicitud Post para agregar un usuario al registro
 	@PostMapping
 	public User create(@RequestBody User user) {
 		return service.save(user);
 	}
 	
+	//Solicitud Put para editar un usuario en especifico 
 	@PutMapping("/{uid}/{id}")
 	public User update(@PathVariable("uid") int uid, @PathVariable("id") int id, @RequestBody User user) {
 		return service.update(uid,id,user);
 	}
 	
+	//Solicitud Delete para eliminar un usuario en especifico
 	@DeleteMapping("/{uid}/{id}")
 	public boolean delete(@PathVariable("uid") int uid, @PathVariable("id") int id) {
 		return service.delete(uid, id);
