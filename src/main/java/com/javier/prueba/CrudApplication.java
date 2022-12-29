89a29d6a79b446c6993143745e445e010322d46f
JaFiGo <orlfigueroagonzalez@gmail.com>
2022-12-29 20:41:11 -0300

    Update CrudApplication.java
    Agregar el json para la persistencia de datos

--- src/main/java/com/javier/prueba/CrudApplication.java
+++ src/main/java/com/javier/prueba/CrudApplication.java
@@ -1,7 +1,18 @@
 package com.javier.prueba;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.javier.prueba.entity.User;
+import com.javier.prueba.service.UserService;
 
 @SpringBootApplication
 public class CrudApplication {
@@ -10,4 +21,19 @@ public class CrudApplication {
 		SpringApplication.run(CrudApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner (UserService userService) {
+		return args ->{
+			ObjectMapper mapper = new ObjectMapper();
+			TypeReference<List<User>> typeReference = new TypeReference<List<User>>() {};
+			InputStream inputStream = TypeReference.class.getResourceAsStream("/json/posts.json");
+			try {
+				List<User> usuarios = mapper.readValue(inputStream, typeReference);
+				userService.save(usuarios);
+				System.out.println("Clientes guardados");
+			}catch(IOException e){
+				System.out.println("No se pudo guardar usuarios" + e.getMessage());
+			}
+		};
+	}
 }
